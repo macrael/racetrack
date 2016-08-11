6b62b66f32d14b0a8cd44d309d158aab91c84d3f
MacRae Linton <macrae.linton@gmail.com>
2016-08-10 23:25:08 -0400

    tabs to spaces

--- models.go
+++ models.go
@@ -3,18 +3,18 @@ package main
 import "time"
 
 type Episode struct {
-	Number uint		`json:"number"`
-	Scores []Scored	`json:"scores"`
-	// summary?
+    Number uint     `json:"number"`
+    Scores []Scored `json:"scores"`
+    // summary?
 }
 
 type ScoreType struct {
-	Name string		`json:"name"` // e.g. Cries
-	Effect int  	`json:"effect"` // +/- 5 pts. 
+    Name string     `json:"name"` // e.g. Cries
+    Effect int      `json:"effect"` // +/- 5 pts. 
 }
 
 type Scored struct {
-	Queen *Queen			`json:"queen"`
-	ScoreType *ScoreType	`json:"sore_type"`
-	Timestamp time.Time		`json:"timestamp"`
+    Queen *Queen            `json:"queen"`
+    ScoreType *ScoreType    `json:"sore_type"`
+    Timestamp time.Time     `json:"timestamp"`
 }
--- racetrack.go
+++ racetrack.go
@@ -1,97 +1,97 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"net/http"
+    "fmt"
+    "time"
+    "net/http"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
 func dummySeason() Season {
-	s := Season{}
-	s.Key = "season:2016"
-	s.Year = 2016
-	s.Title = "The one Bob wins"
+    s := Season{}
+    s.Key = "season:2016"
+    s.Year = 2016
+    s.Title = "The one Bob wins"
 
-	bob := Queen{"12", "season:2016", "Bob the drag queen"}
-	chichi := Queen{"13", "season:2016", "Chi chi"}
+    bob := Queen{"12", "season:2016", "Bob the drag queen"}
+    chichi := Queen{"13", "season:2016", "Chi chi"}
 
-	queens := []Queen{bob, chichi}
-	s.Queens = queens
+    queens := []Queen{bob, chichi}
+    s.Queens = queens
 
-	players := []Player{Player{"MacRae", "Come a little closer"}, Player{"Oscar", "The House Always Wins"}}
-	s.Players = players
+    players := []Player{Player{"MacRae", "Come a little closer"}, Player{"Oscar", "The House Always Wins"}}
+    s.Players = players
 
-	scoring := []ScoreType{}
-	cries := ScoreType{"cries", -2}
-	shade := ScoreType{"throws shade", 4}
-	scoring = append(scoring, cries)
-	scoring = append(scoring, shade)
-	s.ScoreTypes = scoring
+    scoring := []ScoreType{}
+    cries := ScoreType{"cries", -2}
+    shade := ScoreType{"throws shade", 4}
+    scoring = append(scoring, cries)
+    scoring = append(scoring, shade)
+    s.ScoreTypes = scoring
 
 
-	one := Episode{}
-	one.Number = 1
-	scores := []Scored{}
-	scores = append(scores, Scored{&bob, &cries, time.Now()})
-	one.Scores = scores
+    one := Episode{}
+    one.Number = 1
+    scores := []Scored{}
+    scores = append(scores, Scored{&bob, &cries, time.Now()})
+    one.Scores = scores
 
-	two := Episode{}
-	two.Number = 2
-	scores = []Scored{}
-	scores = append(scores, Scored{&bob, &shade, time.Now()})
-	scores = append(scores, Scored{&chichi, &cries, time.Now()})
-	two.Scores = scores
+    two := Episode{}
+    two.Number = 2
+    scores = []Scored{}
+    scores = append(scores, Scored{&bob, &shade, time.Now()})
+    scores = append(scores, Scored{&chichi, &cries, time.Now()})
+    two.Scores = scores
 
-	s.Episodes = []Episode{one, two}
+    s.Episodes = []Episode{one, two}
 
-	return s
+    return s
 }
 
 
 
 func ServeIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Have the site. ")
+    fmt.Println("Have the site. ")
     http.ServeFile(w, r, "./static/index.html")
 }
 
 func main() {
-	fmt.Println("in main")
+    fmt.Println("in main")
 
-	r := mux.NewRouter().StrictSlash(true)
+    r := mux.NewRouter().StrictSlash(true)
 
-	api := r.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/seasons", GetSeasons)
-	// api.HandleFunc("/seasons/current", GetCurrentSeason)
-	api.HandleFunc("/seasons/{season_id}", GetSeason)
-	api.HandleFunc("/seasons/{season_id}/queens", PostQueen).Methods("POST")
-	api.HandleFunc("/seasons/{season_id}/queens/{queen_key}", DeleteQueen).Methods("DELETE")
+    api := r.PathPrefix("/api").Subrouter()
+    api.HandleFunc("/seasons", GetSeasons)
+    // api.HandleFunc("/seasons/current", GetCurrentSeason)
+    api.HandleFunc("/seasons/{season_id}", GetSeason)
+    api.HandleFunc("/seasons/{season_id}/queens", PostQueen).Methods("POST")
+    api.HandleFunc("/seasons/{season_id}/queens/{queen_key}", DeleteQueen).Methods("DELETE")
 
-	r.PathPrefix("/assets").Handler(http.StripPrefix("/assets", http.FileServer(http.Dir("./static/"))))
-	
-	// Serve our single page site to any valid url
-	r.HandleFunc("/", ServeIndex)
-	r.HandleFunc("/queen", ServeIndex)
-	r.HandleFunc("/edit", ServeIndex)
-	r.HandleFunc("/edit/queens", ServeIndex)
+    r.PathPrefix("/assets").Handler(http.StripPrefix("/assets", http.FileServer(http.Dir("./static/"))))
+    
+    // Serve our single page site to any valid url
+    r.HandleFunc("/", ServeIndex)
+    r.HandleFunc("/queen", ServeIndex)
+    r.HandleFunc("/edit", ServeIndex)
+    r.HandleFunc("/edit/queens", ServeIndex)
 
 
-	fmt.Println("testing")
+    fmt.Println("testing")
 
-	s := dummySeason()
-	Seasons = []Season{s}
-	fmt.Println(s)
+    s := dummySeason()
+    Seasons = []Season{s}
+    fmt.Println(s)
 
-	// http.Handle("/", r)
-	http.ListenAndServe(":8080", r)
+    // http.Handle("/", r)
+    http.ListenAndServe(":8080", r)
 
 }
 
 
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("this is good")
-	fmt.Fprintf(w, "Good Morning! %v", Seasons)
+    fmt.Println("this is good")
+    fmt.Fprintf(w, "Good Morning! %v", Seasons)
 }
 
